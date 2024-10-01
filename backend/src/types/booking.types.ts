f159315a84c777197fbfcf1288886f2f55084309
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-10-01 11:48:13 +0300

    add booking- and user related types

--- backend/src/types/booking.types.ts
+++ backend/src/types/booking.types.ts
@@ -2,6 +2,12 @@ import { Room } from './room.types';
 import { User } from './user.types';
 import { Venue } from './venue.types';
 
+export enum BookingStatus {
+  CONFIRMED = 'CONFIRMED',
+  PENDING = 'PENDING',
+  CANCELLED = 'CANCELLED'
+}
+
 export interface Booking {
   id: string;
   startDate: Date;
