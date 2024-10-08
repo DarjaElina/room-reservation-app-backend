273cc92edcbd6ad7e334c8455e215cde8ced943e
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-10-08 15:23:36 +0300

    add migrations for usernumber and username generation

---
+++ backend/src/migrations/20241007_10_user_number_to_users.ts
@@ -0,0 +1,28 @@
+import { DataTypes } from 'sequelize';
+import type { Migration } from '../util/db';
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.addColumn('users', 'user_number', {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  });
+
+  await queryInterface.sequelize.query(`
+    CREATE SEQUENCE users_user_number_seq
+    START WITH 10000;
+  `);
+
+  await queryInterface.sequelize.query(`
+    ALTER TABLE users
+    ALTER COLUMN user_number SET DEFAULT nextval('users_user_number_seq');
+  `);
+  
+};
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.sequelize.query(`
+    DROP SEQUENCE IF EXISTS users_user_number_seq;
+  `);
+  await queryInterface.removeColumn('users', 'user_number');
+};
---
+++ backend/src/migrations/20241007_11_allow_null_username.ts
@@ -0,0 +1,9 @@
+import type { Migration } from '../util/db';
+
+export const up: Migration = async ({ context: queryInterface }) => {
+await queryInterface.sequelize.query(`
+    ALTER TABLE users
+    ALTER COLUMN username DROP NOT NULL;
+  `);
+  
+};
