ee426fd148257da0ec53591463d8180aaf8d7596
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-10-12 22:22:09 +0300

    fix migration file

--- backend/src/migrations/20241007_11_allow_null_username.ts
+++ backend/src/migrations/20241007_11_allow_null_username.ts
@@ -1,9 +1,16 @@
 import type { Migration } from '../util/db';
 
 export const up: Migration = async ({ context: queryInterface }) => {
-await queryInterface.sequelize.query(`
-    ALTER TABLE users
-    ALTER COLUMN username DROP NOT NULL;
-  `);
+  await queryInterface.sequelize.query(`
+      ALTER TABLE users
+      ALTER COLUMN username DROP NOT NULL;
+    `);
   
+};
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.sequelize.query(`
+      ALTER TABLE users
+      ALTER COLUMN username SET NOT NULL;
+    `);
 };
