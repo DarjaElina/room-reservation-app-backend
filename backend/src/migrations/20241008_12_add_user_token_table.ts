5a131ddc0c6abfc53bba510440dfa94f6ac36ae3
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-10-12 22:21:24 +0300

    add migrations for user_token table

---
+++ backend/src/migrations/20241008_12_add_user_token_table.ts
@@ -0,0 +1,45 @@
+import { DataTypes } from 'sequelize';
+import type { Migration } from '../util/db';
+import { tokenTypes } from '../types/token/token.constants';
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable('user_tokens', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    },
+    expires_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM(...tokenTypes),
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  });
+};
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable('user_tokens');
+};
