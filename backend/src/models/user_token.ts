9b48091269aa538eefbb149c14a6af41b32c5739
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-10-12 22:24:15 +0300

    create user_token model

---
+++ backend/src/models/user_token.ts
@@ -0,0 +1,73 @@
+import { Optional } from 'sequelize';
+import { DataType, Table, Model, Column, PrimaryKey, AllowNull, Default, CreatedAt, UpdatedAt, Unique, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { TokenType } from '../types/token/token.enums';
+import { tokenTypes } from '../types/token/token.constants';
+import User from './user';
+
+
+interface UserTokenAttributes {
+  id: string;
+  userId: string;
+  token: string;
+  type: TokenType;
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+}
+
+type UserTokenCreationAttributes = Optional<UserTokenAttributes, 'id' | 'createdAt' | 'updatedAt'>;
+
+@Table({
+  underscored: true,
+  modelName: 'user_token'
+})
+
+class UserToken extends Model<UserTokenAttributes, UserTokenCreationAttributes> {
+  @PrimaryKey
+  @AllowNull(false)
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  id!: string;
+
+  @AllowNull(false)
+  @Unique
+  @Column
+  token!: string;
+  
+  @AllowNull(false)
+  @Column({
+    type: DataType.ENUM(...tokenTypes),
+    validate: {
+      isIn: [tokenTypes],
+    }
+  })
+  type!: TokenType;
+
+  @CreatedAt
+  @Column({
+      type: DataType.DATE,
+      defaultValue: DataType.NOW
+  })
+  createdAt!: Date;
+
+  @UpdatedAt
+  @Column({
+      type: DataType.DATE,
+      defaultValue: DataType.NOW
+  })
+  updatedAt!: Date;
+
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  userId!: number;
+
+  @BelongsTo(() => User)
+  user!: User;
+
+  @AllowNull(false)
+  @Column
+  expiresAt!: Date;
+}
+
+export default UserToken;
