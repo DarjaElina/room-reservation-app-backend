89605945742df247eb965deb4f96a8efef3d5451
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2025-02-05 15:33:58 +0200

    add test for time filter

---
+++ mobile-frontend/src/scripts/getCurrentTime.js
@@ -0,0 +1,8 @@
+const currentDateObj = new Date();
+const currentHour  = currentDateObj.getHours();
+const nextHour  = currentDateObj.setHours(currentDateObj.getHours() + 1);
+
+output.getHours = {
+  currentHour: currentHour.toFixed(),
+  nextHour: nextHour.toFixed(),
+};
